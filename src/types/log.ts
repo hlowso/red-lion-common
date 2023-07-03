dc1b6d983657d122a134732256fd4ae39bf9c7bd
hlowso <h.canderleigh@gmail.com>
2023-07-03 17:04:40 -0400

    added log type

---
+++ src/types/log.ts
@@ -0,0 +1,15 @@
+export type LogVerb = "completed";
+export type SubjectType = "character";
+export type ObjectType = "activity";
+
+export interface Log {
+    id: number,
+    gameId: number,
+    verb: LogVerb,
+    loggedAt: Date,
+    subjectType: SubjectType,
+    subjectId: number,
+    objectType: ObjectType,
+    objectId: number
+    details: any
+}
