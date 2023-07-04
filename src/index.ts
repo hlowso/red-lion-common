b19fb522b4a8f705e65b3c8e2687f418d10ab5cc
hlowso <h.canderleigh@gmail.com>
2023-07-04 18:05:00 -0400

    added new types to index

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ export * from "./types/db";
 export * from "./types/character";
 export * from "./types/activity";
 export * from "./types/log";
+export * from "./types/delta";
+export * from "./types/formula";
 export * from "./constants";
 export * from "./util";
