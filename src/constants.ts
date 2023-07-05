a1f788c4f98e2c3d221b4635bbe66e19a31a5074
hlowso <h.canderleigh@gmail.com>
2023-07-05 12:46:10 -0400

    added socket events to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,6 @@
 export const APP_NAME = "RedLion";
 export const WEEKDAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+export const SOCKET_EVENTS = {
+    IDENTIFY: "identify",
+    NOTIFY: "notify"
+};
