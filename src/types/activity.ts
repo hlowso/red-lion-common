b89580870e0dd0615c2a3df880ef241279379894
hlowso <h.canderleigh@gmail.com>
2023-07-05 22:00:55 -0400

    added activity get params

--- src/types/activity.ts
+++ src/types/activity.ts
@@ -4,3 +4,10 @@ export interface Activity extends ActivityRow {
     listName: string;
     countToday?: number;
 }
+
+export interface ActivityGetParams {
+    id?: number;
+    gameId?: number;
+    listId?: number;
+    characterId?: number;
+}
