03bae18c97cecc9afb78a87153bbd0446078ffc5
hlowso <h.canderleigh@gmail.com>
2023-07-06 21:07:55 -0400

    added character util

---
+++ src/util/character.ts
@@ -0,0 +1,18 @@
+import { Possessions } from "../types/character";
+import { Delta } from "../types/delta";
+
+export const canAffordItem = (possessions: Possessions, costDelta: Delta) => {
+  // Check tallies
+  return !Object.entries(costDelta.tallies).some(([key, change]) => {
+    if (possessions.tallies[key] === undefined) return true;
+
+    if (typeof change === "number" && change < 0)
+      return 0 > possessions.tallies[key] + change;
+
+    if (typeof change === "object") {
+      // TODO: deal with case of formula...
+    }
+
+    return false;
+  });
+};
