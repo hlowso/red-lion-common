75b887ed654cf72d4d3333ae6de780bbe3b81d2e
hlowso <h.canderleigh@gmail.com>
2023-07-07 01:46:47 -0400

    added change types to delta.ts

--- src/types/delta.ts
+++ src/types/delta.ts
@@ -1,13 +1,29 @@
 import { Formula } from "./formula";
 
 export type TallyChanges = {
-    [tallyKey: string]: number | Formula
+  [tallyKey: string]: number | Formula;
+};
+
+export type ItemChanges = {
+  [itemKey: string]: number | Formula;
+};
+
+export type ProgressionChanges = {
+  [progressionKey: string]: boolean;
+};
+
+export type SubscriptionChanges = {
+  [subscriptionKey: string]: "subscribe" | "unsubscribe";
+};
+
+export type StateChanges = {
+  [stateKey: string]: "enter" | "exit";
 };
 
 export interface Delta {
-    tallies: TallyChanges;
-    items: any;
-    progressions: any;
-    subscriptions: any;
-    states: any;
-};
+  tallies?: TallyChanges;
+  items?: ItemChanges;
+  progressions?: ProgressionChanges;
+  subscriptions?: SubscriptionChanges;
+  states?: StateChanges;
+}
