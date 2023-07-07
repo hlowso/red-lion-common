6e0378e53e7e649a8a45bccdca0bfae960e6a776
hlowso <h.canderleigh@gmail.com>
2023-07-07 01:47:24 -0400

    added log verb and object type

--- src/types/log.ts
+++ src/types/log.ts
@@ -1,27 +1,27 @@
-export type LogVerb = "completed";
+export type LogVerb = "completed" | "purchased";
 export type SubjectType = "character";
-export type ObjectType = "activity";
+export type ObjectType = "activity" | "item";
 
 export interface Log {
-    id: number,
-    gameId: number,
-    verb: LogVerb,
-    loggedAt: Date,
-    eventDate: Date,
-    subjectType: SubjectType,
-    subjectId: number,
-    objectType: ObjectType,
-    objectId: number
-    details: any
+  id: number;
+  gameId: number;
+  verb: LogVerb;
+  loggedAt: Date;
+  eventDate: Date;
+  subjectType: SubjectType;
+  subjectId: number;
+  objectType: ObjectType;
+  objectId: number;
+  details: any;
 }
 
 export interface LogGetParams {
-    id?: number;
-    gameId?: number;
-    verb?: LogVerb;
-    subjectType?: SubjectType;
-    subjectId?: number;
-    objectType?: ObjectType;
-    objectId?: number;
-    since?: Date;
+  id?: number;
+  gameId?: number;
+  verb?: LogVerb;
+  subjectType?: SubjectType;
+  subjectId?: number;
+  objectType?: ObjectType;
+  objectId?: number;
+  since?: Date;
 }
