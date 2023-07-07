2c505ec9fefdeff5d2419a82b7ea638ccea12c19
hlowso <h.canderleigh@gmail.com>
2023-07-07 01:47:06 -0400

    added ItemLogObjectRow

--- src/types/item.ts
+++ src/types/item.ts
@@ -2,26 +2,32 @@ import { Delta } from "./delta";
 import { Formula } from "./formula";
 
 export interface ItemRow {
-    id: number;
-    gameId: number;
-    key: string;
-    name: string;
-    description: string;
-    imageId: number;
-    expirationFormula: Formula;
-    costDelta: Delta;
-    use: any;
+  id: number;
+  gameId: number;
+  key: string;
+  name: string;
+  description: string;
+  imageId: number;
+  expirationFormula: Formula;
+  costDelta: Delta;
+  useDelta: Delta;
 }
 
 export interface Item extends ItemRow {
-    imageUrl: string;
+  imageUrl: string;
 }
 
 export interface ItemGetQuery {
-    id?: number;
-    gameId?: number;
+  id?: number;
+  gameId?: number;
 }
 
 export type Items = {
-    [itemKey: string]: number;
+  [itemKey: string]: number;
+};
+
+export interface ItemLogObjectRow {
+  id: number;
+  itemId: number;
+  logId: number;
 }
