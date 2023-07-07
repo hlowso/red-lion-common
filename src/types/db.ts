2bbe31e3d6528b6ee45d954d04b716dac03a919e
hlowso <h.canderleigh@gmail.com>
2023-07-07 01:46:31 -0400

    prettier change to db.ts

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,56 +1,56 @@
 import { Delta } from "./delta";
 
 export interface UserRow {
-    id: number;
-    username: string;
-    email: string;
+  id: number;
+  username: string;
+  email: string;
 }
 
 export interface GameRow {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export interface CharacterRow {
-    id: number;
-    gameId: number;
-    userId: number;
-    imageId: number;
-    items: any;
-    tallies: any;
-    progressions: any;
-    subscriptions: any;
-    states: any;
+  id: number;
+  gameId: number;
+  userId: number;
+  imageId: number;
+  items: any;
+  tallies: any;
+  progressions: any;
+  subscriptions: any;
+  states: any;
 }
 
 export interface ActivityRow {
-    id: number;
-    listId: number;
-    name: string;
-    description?: string;
-    schedule?: string;
-    count?: number;
-    fields?: any;
-    completionDelta: Delta;
+  id: number;
+  listId: number;
+  name: string;
+  description?: string;
+  schedule?: string;
+  count?: number;
+  fields?: any;
+  completionDelta: Delta;
 }
 
 export interface CharacterLogSubjectRow {
-    id: number;
-    characterId: number;
-    logId: number;
+  id: number;
+  characterId: number;
+  logId: number;
 }
 
 export interface ActivityLogObjectRow {
-    id: number;
-    activityId: number;
-    logId: number;
+  id: number;
+  activityId: number;
+  logId: number;
 }
 
 export interface TallyRow {
-    id: number;
-    gameId: number;
-    name: string;
-    key: string;
-    description: string;
-    icon: string;
+  id: number;
+  gameId: number;
+  name: string;
+  key: string;
+  description: string;
+  icon: string;
 }
