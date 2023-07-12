facbaca175a2274cab8f6961c620cc50b1fc40ff
hlowso <h.canderleigh@gmail.com>
2023-07-12 12:20:44 -0400

    added types index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-export * from "./types/db";
-export * from "./types/game";
-export * from "./types/character";
-export * from "./types/list";
-export * from "./types/activity";
-export * from "./types/log";
-export * from "./types/tally";
-export * from "./types/item";
-export * from "./types/delta";
-export * from "./types/formula";
-export * from "./types/web-socket-payloads";
+export * from "./types";
 export * from "./constants";
 export * as Selectors from "./selectors";
 export * as Util from "./util";
