6b7131d17ba07462a0c5fbb954eb9a943af2c458
hlowso <h.canderleigh@gmail.com>
2023-07-17 01:21:14 -0400

    moved listNav util here

--- src/util/list.ts
+++ src/util/list.ts
@@ -1,4 +1,14 @@
 import { ListRow } from "../types";
+import { interfaceLists } from "../selectors";
 
 export const unplannedList = (lists: ListRow[]) =>
   lists.find((l) => l.name === "Unplanned");
+
+export const listNavItems = (lists: ListRow[]) => [
+  { id: -1, name: "Today", icon: "BrightnessLowFill" },
+  ...interfaceLists(lists || []).map(({ id, name }) => ({
+    id,
+    name,
+    icon: "ListCheck",
+  })),
+];
