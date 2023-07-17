f8a356492b6ca4d905663a4c0a267e0c45c9134f
hlowso <h.canderleigh@gmail.com>
2023-07-17 19:24:38 -0400

    moved unplanned to selectors/activities

--- src/selectors/activities.ts
+++ src/selectors/activities.ts
@@ -1,18 +1,16 @@
-import { WEEKDAYS } from "../constants";
+import { ListRow } from "../types";
 import { Activity, ActivityRow } from "../types/activity";
+import { unplannedList } from "../util/list";
+import { dueToday as activityDueToday } from "../util/activity";
 
 export const dueToday = (activities: ActivityRow[]) =>
-  activities
-    .filter((a) => !!a.schedule)
-    .filter(
-      ({ schedule }) =>
-        schedule === "D" ||
-        (schedule!.startsWith("W") &&
-          schedule!.includes(WEEKDAYS[new Date().getDay()]))
-    );
+  activities.filter(activityDueToday);
 
 export const todayComplete = (activities: Activity[]) =>
   activities.filter((a) => (a.count || 1) <= (a.countToday || 0));
 
 export const todayIncomplete = (activities: Activity[]) =>
   activities.filter((a) => (a.count || 1) > (a.countToday || 0));
+
+export const unplanned = (lists: ListRow[], activities: ActivityRow[]) =>
+  activities.filter((a) => a.listId === unplannedList(lists)?.id);
