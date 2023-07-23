43832aae558975d54b8c31734971ca2f7db792e1
hlowso <h.canderleigh@gmail.com>
2023-07-23 13:40:22 -0400

    added isCron tests

--- src/util/date.test.js
+++ src/util/date.test.js
@@ -1,10 +1,34 @@
 const { describe, expect, test } = require("@jest/globals");
-const { isLate } = require("./date");
+const { isLate, isCron } = require("./date");
 
 describe("Util - Date", () => {
   describe("isLate", () => {
     test("correctly indicates a date is not late if it's today", () => {
       expect(isLate(new Date())).toBe(false);
+    });
+  });
+
+  describe("isCron", () => {
+    test("returns false on undefined", () => {
+      expect(isCron()).toBe(false);
+    });
+    test("returns false on empty string", () => {
+      expect(isCron("")).toBe(false);
+    });
+    test("returns false on null", () => {
+      expect(isCron(null)).toBe(false);
+    });
+    test("returns false on date", () => {
+      expect(isCron(new Date())).toBe(false);
+    });
+    test("returns false on date string", () => {
+      expect(isCron("2012-03-07")).toBe(false);
+    });
+    test("returns true on 'every day'", () => {
+      expect(isCron("0 0 * * *")).toBe(true);
+    });
+    test("returns true on 'mondays wednesdays fridays'", () => {
+      expect(isCron("0 0 * * 1,2,3")).toBe(true);
     });
   });
 });
