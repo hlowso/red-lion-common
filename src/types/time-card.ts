15a46c685f47d8288349815462bacda959d0e4bf
hlowso <h.canderleigh@gmail.com>
2023-09-04 18:54:34 -0400

    modified timecard types

--- src/types/time-card.ts
+++ src/types/time-card.ts
@@ -1,14 +1,23 @@
 export type TimeCardPunchDirection = "in" | "out";
 
+export interface TimeCardRow {
+  id: number;
+  gameId: number;
+  name: string;
+  description: string;
+}
+
 export interface TimeCard {
   characterId: number;
   date: Date;
   clockedTime: number;
   status: TimeCardPunchDirection;
+  lastPunch?: Date;
 }
 
 export interface TimeCardPunchRow {
   id: number;
+  timeCardId: number;
   characterId: number;
   direction: TimeCardPunchDirection;
   punchedAt: Date;
