c028b7ccbfbe937b3ba72bec5f20928382788e62
hlowso <h.canderleigh@gmail.com>
2023-09-13 23:18:52 -0400

    removed today from listnavitems

--- src/util/list.ts
+++ src/util/list.ts
@@ -4,11 +4,9 @@ import { interfaceLists } from "../selectors";
 export const unplannedList = (lists: ListRow[]) =>
   lists.find((l) => l.name === "Unplanned");
 
-export const listNavItems = (lists: ListRow[]) => [
-  { id: -1, name: "Today", icon: "BrightnessLowFill" },
-  ...interfaceLists(lists || []).map(({ id, name }) => ({
+export const listNavItems = (lists: ListRow[]) =>
+  interfaceLists(lists || []).map(({ id, name }) => ({
     id,
     name,
     icon: "ListCheck",
-  })),
-];
+  }));
